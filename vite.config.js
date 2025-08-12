226f848482abb023377b0ee79cf227d81256309b
Jake B <jakeburke1775@gmail.com>
2025-08-12 11:10:30 -0500

    config for pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/diverlee/', // Set this to your repo name for GitHub Pages
   plugins: [react()],
   server: {
     port: 3000,
