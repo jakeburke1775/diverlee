8d75c7e4f8ca215ecee139ccf8e2182a3c33eabb
Jake B <jakeburke1775@gmail.com>
2025-08-23 18:28:45 -0500

    Commit all current changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import Hero from './components/Hero/Hero';
-import Services from './components/Services/Services';
-import Footer from './components/Footer/Footer';
+import React from "react";
+import Hero from "./components/Hero/Hero";
+import Services from "./components/Services/Services";
+import Footer from "./components/Footer/Footer";
+import Logo from "./components/Logo/Logo";
 
 function App() {
   return (
     <div className="min-h-screen bg-white">
-      <Hero />
+      <Logo />
+      {/* <Hero />
       <Services />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
